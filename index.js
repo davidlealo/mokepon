168d412c617af12023fbfa29905886b66611b4f4
davidlealo <davidlealo@gmail.com>
2025-03-12 22:22:27 -0300

    solución al desafío

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function seleccionarMascota(){
-    alert('Mascota seleccionada')
-}
+    if (document.getElementById('hipodoge').checked){
+        alert('Has seleccionado a Hipodoge')
+    } else if (document.getElementById('capipepo').checked){
+        alert('Has seleccionado a Capipepo')
+    } else if (document.getElementById('ratigueya').checked){
+        alert('Has seleccionado a Ratigueya')
+} else{
+    alert('Debes seleccionar una mascota')
+}}
 
-let botonMascota = document.getElementById('botonMascota')
+let botonMascota = document.getElementById('boton-mascota')
 botonMascota.addEventListener('click', seleccionarMascota)
