cec3b495785560931bb6bd82a4a2629dae5e1a8a
davidlealo <davidlealo@gmail.com>
2025-03-12 22:06:33 -0300

    prueba funcion

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert("Hola")
+function seleccionarMascota(){
+    alert('Mascota seleccionada')
+}
+
+let botonMascota = document.getElementById('botonMascota')
+botonMascota.addEventListener('click', seleccionarMascota)
