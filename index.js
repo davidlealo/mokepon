ad3181246ac9f649858de8e062c0f1b708406a3e
davidlealo <davidlealo@gmail.com>
2025-03-15 21:54:10 -0300

    enemigo aleatorio

--- index.js
+++ index.js
@@ -7,10 +7,28 @@ function seleccionarMascota(){
         spanMascotaJugador.innerHTML = 'Ratigueya'
 } else{
     alert('Debes seleccionar una mascota')
-}}
+}
+    seleccionarMascotaEnemigo()
+}
 
 function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function seleccionarMascotaEnemigo(){
+    let enemigoAleatorio = aleatorio(1, 3)
+
+    if (enemigoAleatorio === 1){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    }
+    else if (enemigoAleatorio === 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    }
+    else if (enemigoAleatorio === 3){
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    } else{
+        alert('Error al seleccionar la mascota enemiga')
+    }
 }
 
 let hipodoge = document.getElementById('hipodoge')
@@ -18,6 +36,7 @@ let capipepo = document.getElementById('capipepo')
 let ratigueya = document.getElementById('ratigueya')
 
 let spanMascotaJugador = document.getElementById('mascota-jugador')
+let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
 let botonMascota = document.getElementById('boton-mascota')
 botonMascota.addEventListener('click', seleccionarMascota)
